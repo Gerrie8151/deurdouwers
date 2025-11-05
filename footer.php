03fce608017efa276de250c486178c23971ae323
gerrie8151 <remco_gerritse@zeelandnet.nl>
2025-11-05 09:33:53 +0100

    titel en youtube

--- footer.php
+++ footer.php
@@ -2,8 +2,9 @@
 <footer class="bg-gradient-to-r from-yellow-400 to-green-600 text-white text-center py-8 mt-12">
   <p class="text-lg">&copy; <?php echo date("Y"); ?> Carnavalsvereniging De Deurdouwers</p>
   <p class="mt-2">Volg ons op 
-    <a href="https://www.facebook.com/dedeurdouwers.betekoppenstad/?locale=nl_NL" class="underline hover:text-yellow-200">Facebook</a> & 
-    <a href="https://www.instagram.com/dedeurdouwers/" class="underline hover:text-yellow-200">Instagram</a>
+    <a href="https://www.facebook.com/dedeurdouwers.betekoppenstad/?locale=nl_NL" class="underline hover:text-yellow-200">Facebook</a> , 
+    <a href="https://www.instagram.com/dedeurdouwers/" class="underline hover:text-yellow-200">Instagram</a> & 
+    <a href="https://www.youtube.com/@DeDeurdouwers" class="underline hover:text-yellow-200">Youtube</a>
   </p>
 </footer>
 </body>
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="icon" href="https://www.deurdouwers.com/favicon/apple-touch-icon.png">
-  <title>De Deurdouwers</title>
+  <title>C.V De Deurdouwers</title>
 
   <!-- Tailwind -->
   <script src="https://cdn.tailwindcss.com"></script>
